13e6932edf183c48835a70fbd8089d72441c8f69
Marcus L. Jensen <lomztein4@gmail.com>
2021-02-26 16:08:13 +0100

    Frontend is fully functional with backend mock pods

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const express = require("express")
 const app = express()
 
+var mockPods = []
+var idCounter = 0
+
+var randNameStart = [
+    "colossal","brief","absorbing","psychotic","synonymous","handsomely","jagged","labored","fertile","separate","short","chilly","tall","jaded","unkempt","cagey","slim","curious","regular","vast","mature","black-and-white","historical","alleged","teeny","embarrassed","nonchalant","aromatic","few","acidic","shaggy","protective","savory","colorful","dazzling","crooked","good","moaning","stormy","momentous"
+]
+
+var randNameEnd = [
+    "juice","rings","vest","horse","representative","flowers","quicksand","sleep","pest","fact","increase","rule","ice","crayon","stream","dolls","activity","laborer","plot","letters","wish","jam","hour","mist","chin","trail","temper","tank","straw","sea","grip","reason","suggestion","leg","plant","ink","way","grass","bushes","walk"
+]
+
+function generateMockPod () {
+    let pod = 
+    {
+        id: idCounter,
+        ip: "127.0.0." + Math.floor(Math.random() * 255 + 1),
+        domainName: generateRandomName()
+    }
+    idCounter++;
+    return pod;
+}
+
+function generateRandomName () {
+    let start = randNameStart[Math.floor(Math.random() * randNameStart.length)]
+    let mid = randNameStart[Math.floor(Math.random() * randNameStart.length)]
+    let end = randNameEnd[Math.floor(Math.random() * randNameEnd.length)]
+    return start + "-" + mid + "-" + end
+}
+
+function deleteMockPod (id) {
+    mockPods = mockPods.filter(pod => !(pod.id == id));
+}
+
 app.use(express.static("public"))
 app.use(express.json())
 
@@ -14,11 +47,15 @@ app.use("/index", (req, res) => {
 
 app.delete("/deletepod", (req,res) => {
     console.log("deletes pod")
+    console.log(req.body)
+    let pod = req.body
+    deleteMockPod(pod.id)
     res.send("pod deleted!")
 })
 
 app.post("/createpod", (req, res) => {
     console.log("creates pod")
+    mockPods.push(generateMockPod());
     res.send("pod created")
 })
 
@@ -27,12 +64,8 @@ app.get("/test", (req,res) => {
 })
 
 app.get("/pods", (req,res) => {
-    let mockPod1 = {id: 181239123, ip: "127.0.0.1", domainName: "something"}
-    let mockPod2 = {id: 181212312323, ip: "127.0.0.1", domainName: "somasawdhing"}
-    let mockPod3 = {id: 18689239123, ip: "127.0.0.1", domainName: "soadddding"}
-    let mockPod4 = {id: 181239123, ip: "127.0.0.1", domainName: "someawdawdhing"}
-    let mockPodArray = [mockPod1, mockPod2, mockPod3, mockPod4]
-    res.send(mockPodArray)
+    console.log(mockPods);
+    res.send(mockPods)
 })
 
 app.listen("3001", "0.0.0.0", () => {
