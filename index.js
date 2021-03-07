98e910df04988475925d5d1eb88d87fe7dff9372
FrederikBRoth <FrederikBRoth>
2021-03-07 13:07:22 +0100

    shitty pod test

--- index.js
+++ index.js
@@ -1,75 +1,85 @@
 const express = require("express")
 const app = express()
-const {createPod} = require("./kubernetes-handler.js")
+const { createPod, getPods, execGetReq, deletePod, readSpecificPod } = require("./kubernetes-handler.js")
 
 var mockPods = []
 var idCounter = 0
 
 var randNameStart = [
-    "colossal","brief","absorbing","psychotic","synonymous","handsomely","jagged","labored","fertile","separate","short","chilly","tall","jaded","unkempt","cagey","slim","curious","regular","vast","mature","black-and-white","historical","alleged","teeny","embarrassed","nonchalant","aromatic","few","acidic","shaggy","protective","savory","colorful","dazzling","crooked","good","moaning","stormy","momentous"
+    "colossal", "brief", "absorbing", "psychotic", "synonymous", "handsomely", "jagged", "labored", "fertile", "separate", "short", "chilly", "tall", "jaded", "unkempt", "cagey", "slim", "curious", "regular", "vast", "mature", "black-and-white", "historical", "alleged", "teeny", "embarrassed", "nonchalant", "aromatic", "few", "acidic", "shaggy", "protective", "savory", "colorful", "dazzling", "crooked", "good", "moaning", "stormy", "momentous"
 ]
 
 var randNameEnd = [
-    "juice","rings","vest","horse","representative","flowers","quicksand","sleep","pest","fact","increase","rule","ice","crayon","stream","dolls","activity","laborer","plot","letters","wish","jam","hour","mist","chin","trail","temper","tank","straw","sea","grip","reason","suggestion","leg","plant","ink","way","grass","bushes","walk"
+    "juice", "rings", "vest", "horse", "representative", "flowers", "quicksand", "sleep", "pest", "fact", "increase", "rule", "ice", "crayon", "stream", "dolls", "activity", "laborer", "plot", "letters", "wish", "jam", "hour", "mist", "chin", "trail", "temper", "tank", "straw", "sea", "grip", "reason", "suggestion", "leg", "plant", "ink", "way", "grass", "bushes", "walk"
 ]
 
-function generateMockPod () {
-    let pod = 
-    {
-        id: idCounter,
-        ip: "127.0.0." + Math.floor(Math.random() * 255 + 1),
-        domainName: generateRandomName()
-    }
-    idCounter++;
-    return pod;
-}
-
-function generateRandomName () {
+function generateRandomName() {
     let start = randNameStart[Math.floor(Math.random() * randNameStart.length)]
     let mid = randNameStart[Math.floor(Math.random() * randNameStart.length)]
     let end = randNameEnd[Math.floor(Math.random() * randNameEnd.length)]
     return start + "-" + mid + "-" + end
 }
 
-function deleteMockPod (id) {
-    mockPods = mockPods.filter(pod => !(pod.id == id));
-}
-
 app.use(express.static("public"))
 app.use(express.json())
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.redirect("/index")
+
 })
 
 app.use("/index", (req, res) => {
     res.sendFile(__dirname + "/public/html/index.html")
 })
 
-app.delete("/deletepod", (req,res) => {
-    console.log("deletes pod")
+app.delete("/deletepod", async (req, res) => {
     console.log(req.body)
-    let pod = req.body
-    deleteMockPod(pod.id)
-    res.send("pod deleted!")
+    let podName = await req.body.name
+    let response = await deletePod(podName)
+    res.send(response)
 })
 
-app.post("/createpod", (req, res) => {
+app.post("/createpod", async (req, res) => {
+    const name = generateRandomName()
+    await createPod(name)
+    async function wait(ms) {
+        return new Promise(resolve => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    let ipNotSet = true
+    let podResponse
+    while (ipNotSet) {
+        await wait(1000)
+        podResponse = await readSpecificPod(name)
+        if (podResponse.body.status.podIP != undefined) {
+            ipNotSet = false
+        }
+    }
+
     console.log("creates pod")
-    mockPods.push(generateMockPod());
-    res.send("pod created")
+    res.send({ ip: podResponse.body.status.podIP, domainName: podResponse.body.metadata.name })
 })
 
-app.get("/test", (req,res) => {
-    res.send("hej")
-    createPod()
+app.post("/ping", async (req, res) => {
+
+    let count = await execGetReq(req.body.name, req.body.ip)
+    res.send({ count: count })
 })
 
-app.get("/pods", (req,res) => {
-    console.log(mockPods);
-    res.send(mockPods)
+app.get("/updatepings", async (req, res) => {
+    let pods = await getPods()
+    for (const pod of pods) {
+        if (pod.container == "kubertest") {
+            console.log(pod)
+            let count = await execGetReq(pod.domainName, pod.ip, "/status")
+            pod.count = count
+        }
+    }
+    res.send(pods)
 })
 
-app.listen("30", "0.0.0.0", () => {
+app.listen("3001", "0.0.0.0", () => {
     console.log("Server is up!")
 })
--- kubernetes-handler.js
+++ kubernetes-handler.js
@@ -1,22 +1,76 @@
 const k8s = require('@kubernetes/client-node');
 const { V1Container, V1Pod } = require("@kubernetes/client-node");
-
+const { stat } = require('fs');
+const WebSocket = require('ws')
+const fs = require('fs')
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
 const appsV1Api = kc.makeApiClient(k8s.AppsV1Api);
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-k8sApi.listNamespacedPod('default').then((res) => {
-    console.log(res.body.items);
-});
 
-function createPod() {
-    k8sApi.createNamespacedPod('default', {
+async function getPods() {
+    let respond = await k8sApi.listNamespacedPod('default')
+    let podList = []
+    for (const element of respond.body.items) {
+        if (element.spec.containers[0].name == "kubertest") {
+            let domainName = element.metadata.name
+            let ip = element.status.podIP
+            let id = element.metadata.uid
+
+            let pod = { id: id, ip: ip, domainName: domainName, count: 0 }
+            podList.push(pod)
+        }
+    }
+    return podList
+}
+
+async function readSpecificPod(name) {
+    let response = await k8sApi.readNamespacedPod(name, 'default')
+    return response
+}
+
+async function execGetReq(podName, podIP, endpoint = "") {
+    if (podIP != undefined) {
+        const execPromise = new Promise((resolve, reject) => {
+            url = 'wss://127.0.0.1:32768/api/v1/namespaces/default/pods/' + podName + '/exec?stdout=true&stderr=false&stdin=true&tty=true&command=curl&command=http://' + podIP + ':5000' + endpoint + '&container=kubertest'
+            console.log(url)
+            sock = new WebSocket(url, {
+                ca: fs.readFileSync(`${process.env.HOME}/.minikube/ca.crt`),
+                cert: fs.readFileSync(`${process.env.HOME}/.minikube/profiles/minikube/client.crt`),
+                key: fs.readFileSync(`${process.env.HOME}/.minikube/profiles/minikube/client.key`)
+            });
+            sock.on('message', x => {
+                let count = x.toString()
+                if (count != "\u0001") {
+                    let reformatedCount = count.replace("\u0001", "")
+                    resolve(reformatedCount)
+                }
+            });
+        })
+        let count = await execPromise
+        return count
+    } else {
+        return 0
+    }
+
+}
+
+
+//k8sApi.deleteNamespacedPod()
+
+async function deletePod(name) {
+    let response = await k8sApi.deleteNamespacedPod(name, "default", undefined, undefined, 0)
+    return response;
+}
+
+async function createPod(name) {
+    let response = await k8sApi.createNamespacedPod('default', {
         apiVersion: "v1",
         kind: "Pod",
         metadata: {
-            name: "userpod-15"
+            name: name
         },
         spec: {
             containers: [
@@ -31,9 +85,12 @@ function createPod() {
                 }
             ]
         }
-    }).then(test => {
-        console.log(test.body)
-    }) 
+    })
+    return response
 }
 
 module.exports.createPod = createPod
+module.exports.getPods = getPods
+module.exports.execGetReq = execGetReq
+module.exports.deletePod = deletePod
+module.exports.readSpecificPod = readSpecificPod
